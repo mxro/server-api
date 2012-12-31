4ab4556170f949e343e03009c21225679450d58e
mxro <mxro@nowhere.com>
2012-12-31 15:51:51 +1300

    removing thread safeness from default component manager

--- src/main/java/de/mxro/server/internal/DefaultComponentManager.java
+++ src/main/java/de/mxro/server/internal/DefaultComponentManager.java
@@ -23,10 +23,10 @@ public class DefaultComponentManager implements ComponentManager {
 	public ServerComponent addComponent(final int index,
 			final ComponentContext context, final ComponentConfiguration conf) {
 		final ServerComponent component;
-		synchronized (components) {
-			component = createComponent(context, conf);
-			components.add(index, component);
-		}
+
+		component = createComponent(context, conf);
+		components.add(index, component);
+
 		return component;
 	}
 
@@ -34,10 +34,10 @@ public class DefaultComponentManager implements ComponentManager {
 	public ServerComponent addComponent(final ComponentContext context,
 			final ComponentConfiguration conf) {
 		final ServerComponent component;
-		synchronized (components) {
-			component = createComponent(context, conf);
-			components.add(component);
-		}
+
+		component = createComponent(context, conf);
+		components.add(component);
+
 		return component;
 	}
 
@@ -45,12 +45,10 @@ public class DefaultComponentManager implements ComponentManager {
 	public ServerComponent addRunningComponent(final ComponentContext context,
 			final ServerComponent component) {
 
-		synchronized (components) {
+		components.add(component);
+		component.injectContext(context);
+		this.running.add(component);
 
-			components.add(component);
-			component.injectContext(context);
-			this.running.add(component);
-		}
 		return component;
 	}
 
@@ -81,43 +79,38 @@ public class DefaultComponentManager implements ComponentManager {
 	@Override
 	public void startComponent(final String componentId,
 			final StartCallback callback) {
-		synchronized (components) {
 
-			final ServerComponent component = getComponent(componentId);
+		final ServerComponent component = getComponent(componentId);
 
-			if (component == null) {
-				callback.onFailure(new IllegalStateException(
-						"No server with id [" + componentId + "] is defined."));
-				return;
-			}
-
-			synchronized (running) {
-				if (running.contains(component)) {
-					callback.onFailure(new IllegalStateException(
-							"Cannot start an already running server ["
-									+ componentId + "]"));
-					return;
-				}
-
-			}
-
-			component.start(new StartCallback() {
-
-				@Override
-				public void onStarted() {
-					synchronized (running) {
-						running.add(component);
-					}
-					callback.onStarted();
-				}
-
-				@Override
-				public void onFailure(final Throwable t) {
-					callback.onFailure(t);
-				}
-			});
-
+		if (component == null) {
+			callback.onFailure(new IllegalStateException("No server with id ["
+					+ componentId + "] is defined."));
+			return;
 		}
+
+		if (running.contains(component)) {
+			callback.onFailure(new IllegalStateException(
+					"Cannot start an already running server [" + componentId
+							+ "]"));
+			return;
+		}
+
+		component.start(new StartCallback() {
+
+			@Override
+			public void onStarted() {
+
+				running.add(component);
+
+				callback.onStarted();
+			}
+
+			@Override
+			public void onFailure(final Throwable t) {
+				callback.onFailure(t);
+			}
+		});
+
 	}
 
 	private List<String> getComponentIds() {
@@ -132,84 +125,78 @@ public class DefaultComponentManager implements ComponentManager {
 	@Override
 	public void stopComponent(final String componentId,
 			final ShutdownCallback callback) {
-		synchronized (components) {
 
-			final ServerComponent component = getComponent(componentId);
+		final ServerComponent component = getComponent(componentId);
 
-			if (component == null) {
-				callback.onFailure(new IllegalStateException(
-						"No server component with id [" + componentId
-								+ "] is defined. Defined components are: "
-								+ getComponentIds()));
-				return;
-			}
-
-			synchronized (running) {
-				if (!running.contains(component)) {
-					callback.onFailure(new IllegalStateException(
-							"Cannot stop an not running server [" + componentId
-									+ "]"));
-					return;
-				}
-			}
-
-			component.stop(new ShutdownCallback() {
-
-				@Override
-				public void onShutdownComplete() {
-					synchronized (running) {
-						running.remove(component);
-					}
-					callback.onShutdownComplete();
-				}
-
-				@Override
-				public void onFailure(final Throwable t) {
-					callback.onFailure(t);
-				}
-			});
+		if (component == null) {
+			callback.onFailure(new IllegalStateException(
+					"No server component with id [" + componentId
+							+ "] is defined. Defined components are: "
+							+ getComponentIds()));
+			return;
 		}
+
+		if (!running.contains(component)) {
+			callback.onFailure(new IllegalStateException(
+					"Cannot stop an not running server [" + componentId + "]"));
+			return;
+		}
+
+		component.stop(new ShutdownCallback() {
+
+			@Override
+			public void onShutdownComplete() {
+
+				running.remove(component);
+
+				callback.onShutdownComplete();
+			}
+
+			@Override
+			public void onFailure(final Throwable t) {
+				callback.onFailure(t);
+			}
+		});
+
 	}
 
 	@Override
 	public int removeComponent(final String componentId) {
-		synchronized (components) {
-			final ServerComponent component = getComponent(componentId);
-			if (component == null) {
-				throw new IllegalStateException("No server with the id ["
-						+ componentId + "] is defined.");
-			}
 
-			synchronized (running) {
-				if (running.contains(component)) {
-					throw new IllegalStateException(
-							"Cannot remove server. Server must first be stopped ["
-									+ componentId + "]");
-				}
-			}
-
-			final int idx = components.indexOf(component);
-
-			if (idx < 0) {
-				throw new IllegalStateException(
-						"Cannot remove server that is not defined.");
-			}
-
-			components.remove(component);
-			return idx;
+		final ServerComponent component = getComponent(componentId);
+		if (component == null) {
+			throw new IllegalStateException("No server with the id ["
+					+ componentId + "] is defined.");
 		}
+
+		if (running.contains(component)) {
+			throw new IllegalStateException(
+					"Cannot remove server. Server must first be stopped ["
+							+ componentId + "]");
+		}
+
+		final int idx = components.indexOf(component);
+
+		if (idx < 0) {
+			throw new IllegalStateException(
+					"Cannot remove server that is not defined.");
+		}
+
+		components.remove(component);
+		return idx;
+
 	}
 
 	@Override
 	public ServerComponent getComponent(final String componentId) {
-		synchronized (components) {
-			for (final ServerComponent comp : components) {
-				if (comp.getConfiguration().getId().equals(componentId)) {
-					return comp;
-				}
+
+		for (final ServerComponent comp : components) {
+			if (comp.getConfiguration().getId().equals(componentId)) {
+				return comp;
 			}
-			return null;
 		}
+		return null;
+
 	}
 
 	public DefaultComponentManager(final ComponentFactory factory) {
@@ -218,6 +205,7 @@ public class DefaultComponentManager implements ComponentManager {
 
 		this.components = new LinkedList<ServerComponent>();
 		this.running = new LinkedList<ServerComponent>();
+
 	}
 
 	@Override
