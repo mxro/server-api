59d640196b0830cb1f26822655bc359cce87b123
mxro <mxro@nowhere.com>
2014-07-05 14:01:55 +1200

    Refactoring Appjangle Server (nxAppCloudNode) to make it more portable and usable for tests Autocommit Sat Jul 05 14:01:55 NZST 2014

--- src/main/java/de/mxro/server/ComponentConfiguration.java
+++ src/main/java/de/mxro/server/ComponentConfiguration.java
@@ -11,6 +11,17 @@ import de.mxro.factories.Configuration;
  */
 public interface ComponentConfiguration extends Configuration {
 
-	
+	/**
+	 * Is this a critical component for the server or is it a background server, which can be shut down asynchronously.
+	 * @return
+	 */
+	public boolean isBackgroundService();
+
+	/**
+	 * A unique ID for this component within the server it is running in.
+	 * 
+	 * @return
+	 */
+	public String getId();
 
 }
