b87860ac53dc656577cee0e3565fb8aef0dce66c
mxro <mxro@nowhere.com>
2014-07-30 11:26:03 +1200

    a

---
+++ src/main/java/de/mxro/server/contexts/LocalStatefulContextConfiguration.java
@@ -0,0 +1,5 @@
+package de.mxro.server.contexts;
+
+public interface LocalStatefulContextConfiguration extends StatefulContextConfiguration {
+
+}
---
+++ src/main/java/de/mxro/server/contexts/StatefulContextConfiguration.java
@@ -0,0 +1,7 @@
+package de.mxro.server.contexts;
+
+import de.mxro.factories.Configuration;
+
+public interface StatefulContextConfiguration extends Configuration {
+
+}
