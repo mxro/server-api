1afff1a546120c9d060602362e3cc9a8dc1c3a1f
mxro <mxro@nowhere.com>
2015-01-15 09:25:18 +1100

    Linking logs, properties and metrics to server Autocommit Thu Jan 15 09:25:18 EST 2015

--- src/main/java/de/mxro/server/ServerApi.java
+++ src/main/java/de/mxro/server/ServerApi.java
@@ -10,81 +10,73 @@ import de.mxro.server.contexts.LogCallback;
 import de.mxro.server.contexts.StatefulContext;
 import de.mxro.server.contexts.StatefulContextConfiguration;
 import de.mxro.server.internal.DefaultComponentManager;
-import de.mxro.server.internal.LocalStatefulContext;
 import de.mxro.server.internal.LocalStatefulContextFactory;
 import de.mxro.server.manager.ComponentManager;
 import de.mxro.service.callbacks.ShutdownCallback;
 
 public class ServerApi {
 
-	/**
-	 * <p>
-	 * A default manager for components.
-	 * </p>
-	 * <p>
-	 * Beware this manager is not thread safe.
-	 * </p>
-	 * 
-	 * @param factory
-	 * @return
-	 */
-	public static ComponentManager createManager(
-			final FactoryCollection factories) {
-		return new DefaultComponentManager(factories);
+    /**
+     * <p>
+     * A default manager for components.
+     * </p>
+     * <p>
+     * Beware this manager is not thread safe.
+     * </p>
+     * 
+     * @param factory
+     * @return
+     */
+    public static ComponentManager createManager(final FactoryCollection factories) {
+        return new DefaultComponentManager(factories);
 
-	}
+    }
 
-	public static LogCallback logCallback(final Object source) {
-		return new LogCallback() {
+    public static LogCallback logCallback(final Object source) {
+        return new LogCallback() {
 
-			@Override
-			public void onLogged() {
+            @Override
+            public void onLogged() {
 
-			}
+            }
 
-			@Override
-			public void onFailure(final Throwable t) {
-				throw new RuntimeException("Log call failed from: " + source, t);
-			}
-		};
-	}
+            @Override
+            public void onFailure(final Throwable t) {
+                throw new RuntimeException("Log call failed from: " + source, t);
+            }
+        };
+    }
 
-	public static StatefulContext createLocalStatefulContext() {
-		return new LocalStatefulContext();
-	}
+    public static Factory<? extends StatefulContext, ? extends StatefulContextConfiguration, Dependencies> createLocalStatefulContextFactory() {
+        return new LocalStatefulContextFactory();
+    }
 
-	public static Factory<? extends StatefulContext, ? extends StatefulContextConfiguration, Dependencies> createLocalStatefulContextFactory() {
-		return new LocalStatefulContextFactory();
-	}
+    public static void performShutdown(final List<ServerComponent> toShutdown, final ShutdownCallback callback) {
 
-	public static void performShutdown(final List<ServerComponent> toShutdown,
-			final ShutdownCallback callback) {
+        if (toShutdown.size() == 0) {
+            callback.onSuccess();
+            return;
+        }
 
-		if (toShutdown.size() == 0) {
-			callback.onSuccess();
-			return;
-		}
+        final ServerComponent server = toShutdown.get(0);
+        toShutdown.remove(0);
 
-		final ServerComponent server = toShutdown.get(0);
-		toShutdown.remove(0);
+        final List<ServerComponent> remainingServers = new ArrayList<ServerComponent>(toShutdown);
+        server.stop(new ShutdownCallback() {
 
-		final List<ServerComponent> remainingServers = new ArrayList<ServerComponent>(
-				toShutdown);
-		server.stop(new ShutdownCallback() {
+            @Override
+            public void onSuccess() {
+                performShutdown(remainingServers, callback);
+            }
 
-			@Override
-			public void onSuccess() {
-				performShutdown(remainingServers, callback);
-			}
+            @Override
+            public void onFailure(final Throwable t) {
+                t.printStackTrace();
+                performShutdown(remainingServers, callback);
+                callback.onFailure(t);
+            }
+        });
 
-			@Override
-			public void onFailure(final Throwable t) {
-				t.printStackTrace();
-				performShutdown(remainingServers, callback);
-				callback.onFailure(t);
-			}
-		});
-
-	}
+    }
 
 }
